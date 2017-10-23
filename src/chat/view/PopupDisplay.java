2ed7f0b31f2824abc16946e2e323ed4d2afb2473
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-23 12:01:48 -0600

    adjusted gitignore files and fixed  the repository. Added a image for the chatbot and a file for that image to go into.

---
+++ src/chat/view/PopupDisplay.java
@@ -0,0 +1,28 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+public class PopupDisplay 
+{
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("chat.view.images/template guy.png"));
+		windowTitle = "Chatbot says";
+	}
+	
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
+	}
+	
+	public String getResponse(String question)
+	{
+		String answer = "";
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
+		return answer;
+	}
+}
