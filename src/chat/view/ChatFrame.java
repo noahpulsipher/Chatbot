f08f606e5c2fe4132ba55874bacd496f20c99576
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-15 12:48:46 -0700

    set up a panel class for chatbot.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -12,7 +12,7 @@ public class ChatFrame extends JFrame
 	{
 		super();
 		this.appController = appController;
-//		appPane = new ChatPanel(appController);
+		appPane = new ChatPanel(appController);
 		setupFrame();
 	}
 	
@@ -20,7 +20,7 @@ public class ChatFrame extends JFrame
 	{
 		this.setSize(600, 600);
 		this.setTitle("Chatting with the chatbot");
-//		this.setContentPane(appPane);
+		this.setContentPane(appPane);
 		this.setResizable(false);
 		this.setVisible(true);
 	}
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,38 @@
 package chat.view;
 
-public class ChatPanel 
-{
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import chat.controller.ChatbotController;
+import java.awt.Color;
 
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setListeners()
+	{
+		
+	}
 }
