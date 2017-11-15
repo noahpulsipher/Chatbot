9945c35faf63ae524b5ecd9b630293f322185677
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-15 12:22:52 -0700

    Commented out code beacause I haven't added the other stuff make it work yet.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -12,16 +12,16 @@ public class ChatFrame extends JFrame
 	{
 		super();
 		this.appController = appController;
-		appPane = new ChatPanel(appController);
+//		appPane = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setContentPane(appPane);
-		this.setTitle("Chatting with the chatbot");
-		this.setResizable(false);
 		this.setSize(600, 600);
+		this.setTitle("Chatting with the chatbot");
+//		this.setContentPane(appPane);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 	
