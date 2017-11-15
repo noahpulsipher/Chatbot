95bffa710d6af76f1fa64699581cb124a81a817c
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-15 12:27:19 -0700

    Declared a variable appFrame.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,16 +2,19 @@ package chat.controller;
 
 import chat.view.PopupDisplay;
 import chat.model.Chatbot;
+import chat.view.ChatFrame;
 
 public class ChatbotController 
 {
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appFrame;
 	
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Noah Pulsipher");
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
 	
 	private String popupChat(String chat)
@@ -27,10 +30,10 @@ public class ChatbotController
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response = display.collectResponse(response);
+//		}
 	}
 }
