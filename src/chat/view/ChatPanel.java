34115d3a4381be4bf60912ce072f76be059dd7f1
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-11-17 12:18:56 -0700

    Created a listener so that when the button is clicked, the text goes away and they can type again.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -7,6 +7,8 @@ import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
 import chat.controller.ChatbotController;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatPanel extends JPanel
 {
@@ -70,6 +72,15 @@ public class ChatPanel extends JPanel
 	 */
 	private void setupListeners()
 	{
-		
+		chatButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				String userText = inputField.getText();
+				String displayText = appController.interactWithChatbot(userText);
+				chatArea.append(displayText);
+				inputField.setText("");
+			}
+		});
 	}
 }
