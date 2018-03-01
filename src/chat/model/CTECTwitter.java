b409147af102df1017824b3265ea2a62282a084e
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-03-01 11:59:06 -0700

    added the collect tweets method to go through all the tweets.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -51,4 +51,45 @@ public class CTECTwitter
 			appController.handleErrors(otherError);
 		}
 	}
+	
+	public String getMostCommonWord(String username)
+	{
+		String mostCommon = "";
+		
+		collectTweets(username);
+		
+		return mostCommon;
+	}
+	
+	private void collectTweets(String username)
+	{
+		searchedTweets.clear();
+		tweetedWords.clear();
+		
+		Paging statusPage = new Paging(1,100);
+		int page = 1;
+		long lastID = Long.MAX_VALUE;
+		
+		while(page <= 10)
+		{
+			statusPage.setPage(page);
+			try
+			{
+				ResponseList<Status> listedTweets = chatbotTwitter.getUserTimeline(username, statusPage);
+				for(Status current : listedTweets)
+				{
+					if(current.getId() < lastID)
+					{
+						searchedTweets.add(current);
+						lastID = current.getId();
+					}
+				}
+			}
+			catch(TwitterException searcTweetError)
+			{
+				appController.handleErrors(searchTweetError);
+			}
+			page++;
+		}
+	}
 }
